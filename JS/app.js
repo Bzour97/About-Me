59496f6d7ff84cc0b77d17e6f9a7917ab0d943dc
Bzour97 <x_a@outlook.com>
2021-08-02 15:09:36 +0300

    create About Me

---
+++ JS/app.js
@@ -0,0 +1,87 @@
+'use strict';
+
+let name = prompt("What is your name")
+
+alert("Hey, I love u " + name )
+
+let favColor = prompt("My fav color is Black ?")
+
+favColor = favColor.toLowerCase();
+
+if(favColor == "yes")
+   {
+   //console.log("This is correct, You are so wonderful and you made my little heart happy")
+   alert("This is correct, You are so wonderful and you made my little heart happy")
+   }
+   
+   else
+   {
+    //console.log("This is a mistake ... try again my friend hehehe")
+    alert("This is a mistake ... try again my friend hehehe") 
+   }
+
+let darker = prompt("Black is darker than White right for me ?")
+
+darker = darker.toLowerCase();
+
+if(darker == "yes")
+    {
+    //console.log("This is correct, You are so wonderful and you made my little heart happy")
+    alert("This is correct, You are so wonderful and you made my little heart happy")
+    }
+    
+    else
+    {
+     //console.log("This is a mistake ... try again my friend hehehe")
+     alert("This is a mistake ... try again my friend hehehe") 
+    }
+
+let cigarette = prompt("Is Winston my favorite cigarette ?")
+
+cigarette = cigarette.toLowerCase();
+
+if(cigarette == "yes")
+    {
+    //console.log("This is correct, You are so wonderful and you made my little heart happy")
+    alert("This is correct, You are so wonderful and you made my little heart happy")
+    }
+    
+    else
+    {
+     //console.log("This is a mistake ... try again my friend hehehe")
+     alert("This is a mistake ... try again my friend hehehe") 
+    }
+
+let bzour = prompt("Ahmad Al-Bzour is a very smart and wonderful person who sanctifies sleep and does not talking to girls ?")
+
+bzour = bzour.toLowerCase();
+
+if(bzour == "yes")
+    {
+    //console.log("This is correct, You are so wonderful and you made my little heart happy")
+    alert("This is correct, You are so wonderful and you made my little heart happy")
+    }
+    
+    else
+    {
+     //console.log("This is a mistake ... try again my friend hehehe")
+     alert("This is a mistake ... try again my friend hehehe") 
+    }
+
+let spongeBob = prompt("Is SpongeBob my favorite character ?")
+
+spongeBob = spongeBob.toLowerCase();
+
+if(spongeBob == "yes")
+    {
+    //console.log("This is correct, You are so wonderful and you made my little heart happy")
+    alert("This is correct, You are so wonderful and you made my little heart happy")
+    }
+    
+    else
+    {
+     //console.log("This is a mistake ... try again my friend hehehe")
+     alert("This is a mistake ... try again my friend hehehe") 
+    }
+
+alert("Thank you for using Umniah haha teaser .. thank you " + name )
