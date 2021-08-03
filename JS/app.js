58422bb2e474f29e193558a927fac1e5a4348a3a
Bzour97 <x_a@outlook.com>
2021-08-03 21:26:33 +0300

    do lab 03

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let mark = 0;
+
 let name = prompt("What is your name")
 
 alert("Hey, I love u " + name )
@@ -10,6 +12,7 @@ favColor = favColor.toLowerCase();
 
 if(favColor == "yes")
    {
+   mark++;
    //console.log("This is correct, You are so wonderful and you made my little heart happy")
    alert("This is correct, You are so wonderful and you made my little heart happy")
    }
@@ -26,6 +29,7 @@ darker = darker.toLowerCase();
 
 if(darker == "yes")
     {
+    mark++;
     //console.log("This is correct, You are so wonderful and you made my little heart happy")
     alert("This is correct, You are so wonderful and you made my little heart happy")
     }
@@ -42,6 +46,7 @@ cigarette = cigarette.toLowerCase();
 
 if(cigarette == "yes")
     {
+    mark++;
     //console.log("This is correct, You are so wonderful and you made my little heart happy")
     alert("This is correct, You are so wonderful and you made my little heart happy")
     }
@@ -58,6 +63,7 @@ bzour = bzour.toLowerCase();
 
 if(bzour == "yes")
     {
+    mark++;
     //console.log("This is correct, You are so wonderful and you made my little heart happy")
     alert("This is correct, You are so wonderful and you made my little heart happy")
     }
@@ -74,6 +80,7 @@ spongeBob = spongeBob.toLowerCase();
 
 if(spongeBob == "yes")
     {
+    mark++;
     //console.log("This is correct, You are so wonderful and you made my little heart happy")
     alert("This is correct, You are so wonderful and you made my little heart happy")
     }
@@ -84,4 +91,72 @@ if(spongeBob == "yes")
      alert("This is a mistake ... try again my friend hehehe") 
     }
 
-alert("Thank you for using Umniah haha teaser .. thank you " + name )
+let sleep = prompt("How many hours do I sleep a day ?")
+
+sleep = sleep.toLowerCase();
+    
+if(sleep == 25)
+    {
+    mark++;
+    //console.log("This is correct, You are so wonderful and you made my little heart happy")
+    alert("This is correct, You are so wonderful and you made my little heart happy, I sleep " + sleep)
+    }
+        
+    else
+    {
+        for(let i = 1; i < 4; i++){
+
+            if(sleep > 25)
+        alert("I exaggerated too much")
+    
+        if(sleep < 25)
+        alert("You hurt me so much")
+    
+        sleep = Number(prompt("Choose between 1, 25, 100"))
+    
+        if(sleep == 25){
+        mark++;
+        alert("It's true I sleep " + sleep )
+        break;}
+        }
+        if(sleep != 25)
+        alert("The number of hours I sleep is 25") 
+    }
+    
+    //console.log("Answer is 25")
+
+    let favFood = prompt("My favorite food is : choose one of these (Bameeh, Jaj hos, Shawerma, Mahashi, Burger, Malfof)").toLowerCase()
+
+    let arrayFood = ['mansaf', 'msakhan', 'shawerma', 'falafel', 'burger', 'sushi'];
+    
+    let boolean = false;
+    
+    for(let j = 1; j < 6; j++){
+    
+    for(let i = 0; i < arrayFood.length; i++){
+    
+     if(favFood == arrayFood[i]){
+        
+        alert("This is correct") 
+        boolean = true;
+        mark++;
+        break;
+     }
+    }
+    
+    if(boolean == false){
+        alert("This is not correct") 
+        favFood = prompt("My favorite food is : choose one of these (Bameeh, Jaj hos, Shawerma, Mahashi, Burger, Malfof)").toLowerCase()
+       
+    }
+    if(boolean == true){
+        mark++;
+        break;
+    }
+    
+    }
+    if(boolean == false){
+        alert("Ooooh shit, the correct answer is : Shawerma, Burger, Mansaf and Msakhan")
+    }
+
+alert("Thank you for using Umniah haha teaser .. thank you " + name + ". Your mark is : " + mark)
